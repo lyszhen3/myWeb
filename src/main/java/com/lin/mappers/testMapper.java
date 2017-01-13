9256cc1a1feccb0d8dce9ffa5fd4685df673bfb1
lyszhen3 <947206869@qq.com>
2017-01-13 17:32:24 +0800

    110
    框架刚完成web工程哦

---
+++ src/main/java/com/lin/mappers/testMapper.java
@@ -0,0 +1,18 @@
+package com.lin.mappers;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Created by pc on 2017-01-13.
+ *
+ * @author pc
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ */
+@Scope("singleton")
+@Lazy(true)
+public interface testMapper {
+
+
+}
---
+++ src/main/java/com/test.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * Created by pc on 2017-01-11.
+ *
+ * @author pc
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
