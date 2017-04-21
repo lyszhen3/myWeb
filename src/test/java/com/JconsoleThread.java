d8cf934c329fcbb288bf7ecaaae31b319db362f1
lyszhen3 <947206869@qq.com>
2017-04-21 17:18:00 +0800

    1111

--- src/test/java/com/JconsoleThread.java
+++ src/test/java/com/JconsoleThread.java
@@ -1,5 +1,9 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by pc on 2017-04-20.
  *
@@ -22,7 +26,56 @@ public class JconsoleThread {
 
     public static void createLockThread(final Object lock){
 
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock){
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
 
+            }
+        },"testLockThread");
+        thread.start();
 
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        createBusyThread();
+        br.readLine();
+        Object obj=new Object();
+        createLockThread(obj);
+
+    }
+
+    static class SynAddRunnable implements Runnable{
+        int a,b;
+
+        public SynAddRunnable(int a,int b){
+            this.a=a;
+            this.b=b;
+        }
+        @Override
+        public void run() {
+            synchronized (Integer.valueOf(a)){
+                synchronized (Integer.valueOf(b)){
+                    System.out.println(a+b+":"+Thread.currentThread().getName());
+                }
+
+            }
+
+        }
+
+        public static void main(String[] args) {
+            for(int i=0;i<100;i++){
+                new Thread(new SynAddRunnable(1,2),"我的1").start();
+                new Thread(new SynAddRunnable(2,1),"我的2").start();
+            }
+        }
     }
 }
---
+++ src/test/java/com/TestDeadLock.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * Created by pc on 2017-04-21.
+ *死锁产生
+ * @author pc
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ */
+public class TestDeadLock {
+
+
+}
