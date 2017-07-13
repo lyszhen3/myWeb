245c11d32d3742bc0353c94bbcd5f1f06c5064d6
lyszhen3 <947206869@qq.com>
2017-07-13 17:39:11 +0800

    更新

--- src/test/java/com/proxy/DynamicProxyTest.java
+++ src/test/java/com/proxy/DynamicProxyTest.java
@@ -14,9 +14,40 @@ import java.lang.reflect.Proxy;
  */
 public class DynamicProxyTest {
 
+    interface IHello{
+        void sayHello();
+    }
+    static class Hello implements IHello{
+
+        @Override
+        public void sayHello() {
+            System.out.println("hello world");
+        }
+    }
+    static class DynamicProxy implements InvocationHandler{
+
+        Object originObj;
+        Object bind(Object originObj){
+            this.originObj = originObj;
+             return Proxy.newProxyInstance(originObj.getClass().getClassLoader(),originObj.getClass().getInterfaces(),this);
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+            System.out.println("welcome");
+            Object invoke = method.invoke(originObj, args);
+            System.out.println("over");
+            return invoke;
+        }
+    }
+
 
 
     public static void main(String[] args) {
+
+
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         IHello hello = (IHello) new DynamicProxy().bind(new Hello());
         hello.sayHello();
     }
