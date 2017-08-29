83a0f561a109d211c8b7f8c6957615ebcdae1664
lyszhen3 <947206869@qq.com>
2017-08-29 13:51:18 +0800

    fix

--- src/test/java/com/linkedDemo/DoubleLinkedList.java
+++ src/test/java/com/linkedDemo/DoubleLinkedList.java
@@ -1,7 +1,6 @@
 package com.linkedDemo;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 import java.util.LinkedList;
 
 /**
@@ -17,57 +16,86 @@ public class DoubleLinkedList<E> {
         E e;
         Node<E> prev;
         Node<E> next;
+
         Node(E e) {
             this.e = e;
         }
     }
+
     private transient Node<E> first;
     private transient Node<E> last;
 
-    public void insertFirst(E e){
+    public void insertFirst(E e) {
         Node<E> node = new Node(e);
-        if(first == null){
+        if (first == null) {
             last = node;
-        }else{
+        } else {
             node.next = first;
             first.prev = node;
         }
-        first  = node;
+        first = node;
 
     }
-    public void insertLast(E e){
+
+    public void insertLast(E e) {
         Node<E> node = new Node<>(e);
-        if(last ==null){
+        if (last == null) {
             first = node;
-        }else{
+        } else {
             node.prev = last;
             last.next = node;
         }
         last = node;
     }
+
     public void deleteFirst() throws Exception {
-        if(first == null){
+        if (first == null) {
             throw new Exception("empty");
         }
-        if(first.next ==null){
+        if (first.next == null) {
             first = null;
             last = null;
-        }else {
+        } else {
             first.next.prev = null;
             first = first.next;
         }
 
     }
+
     public void deleteLast() throws Exception {
-        if(last ==null){
-            throw  new Exception("empty");
+        if (last == null) {
+            throw new Exception("empty");
         }
-        if(last.prev ==null){
-            last =null;
+        if (last.prev == null) {
+            last = null;
             first = null;
-        }else{
-            last.prev.next =null;
+        } else {
+            last.prev.next = null;
             last = last.prev;
+        }
+    }
+
+    public void delete(E e) throws Exception {
+        if (first == null) {
+            throw new Exception("empty");
+        }
+        Node<E> cur = first;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                if (cur == first) {
+                    cur.next.prev = null;
+                    first = cur.next;
+                } else if (cur == last) {
+                    cur.prev.next = null;
+                    last = cur.prev;
+                } else {
+                    cur.prev.next = cur.next;
+                    cur.next.prev = cur.prev;
+                }
+                break;
+
+            }
+            cur = cur.next;
         }
     }
 
@@ -75,28 +103,29 @@ public class DoubleLinkedList<E> {
     /**
      * 从前到后
      */
-    public void displayPtoN(){
-        if(first == null){
+    public void displayPtoN() {
+        if (first == null) {
             System.out.println("empty");
-        }else{
+        } else {
             System.out.print("p-n :");
             Node<E> node = first;
-            while (node!=null){
-                System.out.print(node.e.toString()+"->");
+            while (node != null) {
+                System.out.print(node.e.toString() + "->");
                 node = node.next;
             }
         }
         System.out.print("\n");
 
     }
-    public void displayNtoP(){
-        if(last == null){
+
+    public void displayNtoP() {
+        if (last == null) {
             System.out.println("empty");
-        }else{
+        } else {
             System.out.print("n-p :");
             Node<E> node = last;
-            while (node!=null){
-                System.out.print(node.e.toString()+"->");
+            while (node != null) {
+                System.out.print(node.e.toString() + "->");
                 node = node.prev;
             }
         }
@@ -119,11 +148,33 @@ public class DoubleLinkedList<E> {
         doubleLinkedList.displayPtoN();
         doubleLinkedList.displayNtoP();
         doubleLinkedList.deleteLast();
-         doubleLinkedList.displayPtoN();
+        doubleLinkedList.displayPtoN();
+        doubleLinkedList.displayNtoP();
+        doubleLinkedList.delete(2);
+        doubleLinkedList.displayPtoN();
         doubleLinkedList.displayNtoP();
         //研究list源代码
-         LinkedList<Integer> list = new LinkedList();
-        ArrayList<Integer> arrayList = new ArrayList();
+        LinkedList<Integer> list = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("语文", 1);
+        map.put("数学", 2);
+        map.put("英语", 3);
+        map.put("历史", 4);
+        map.put("政治", 5);
+        map.put("地理", 6);
+        map.put("生物", 7);
+        map.put("化学", 8);
+        map.size();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        int i = 828406^(828406>>> 16);
+
+        int i2 = 682768^(682768>>>16);
+        System.out.println(8&i);
+        System.out.println(8&i2);
+
         arrayList.add(1);
     }
 
