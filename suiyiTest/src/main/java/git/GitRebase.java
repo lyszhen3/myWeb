717bdcaa6180ce7327780dec29bdde33d7c7bdc4
lyszhen3 <lyszhen3@126.com>
2018-06-30 20:54:52 +0800

    合并的0

---
+++ suiyiTest/src/main/java/git/GitRebase.java
@@ -0,0 +1,16 @@
+package git;
+
+/**
+ * Created by lys on 6/30/2018.
+ *
+ * @author lys
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ */
+public abstract class GitRebase {
+
+	public abstract  void commit();
+	public abstract  void commit1();
+	public abstract void commit3();
+	public abstract void commit4();
+}
