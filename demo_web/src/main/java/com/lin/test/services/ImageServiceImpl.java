b7a3140fdcff01b42ffd27d10bfae6851cfb5c34
lyszhen3 <lyszhen3@126.com>
2018-07-02 15:34:19 +0800

    图片制作

---
+++ demo_web/src/main/java/com/lin/test/services/ImageServiceImpl.java
@@ -0,0 +1,13 @@
+package com.lin.test.services;
+
+/**
+ * Created by lys on 7/2/2018.
+ *
+ * @author lys
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ */
+public class ImageServiceImpl implements ImageService{
+
+
+}
