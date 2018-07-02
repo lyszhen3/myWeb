492313758b02c690ea3e2c9cb6c802418e92fa06
lyszhen3 <lyszhen3@126.com>
2018-07-02 15:34:19 +0800

    图片制作

---
+++ demo_web/src/main/java/com/lin/test/services/ImageService.java
@@ -0,0 +1,11 @@
+package com.lin.test.services;
+
+/**
+ * Created by lys on 7/2/2018.
+ *
+ * @author lys
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ */
+public interface ImageService {
+}
