1ad0828fc533e99efbf218c3a0ccc62eb53fdbd4
lyszhen3 <lyszhen3@126.com>
2019-01-11 18:48:38 +0800

    更新

--- suiyiTest/src/main/java/TestClass.java
+++ suiyiTest/src/main/java/TestClass.java
@@ -1,6 +1,7 @@
+import org.apache.commons.mail.HtmlEmail;
+
 import java.io.File;
 import java.util.Objects;
-import java.util.TreeMap;
 
 /**
  * Created by lys on 2018/9/6.
@@ -34,7 +35,33 @@ public class TestClass {
 	}
 
 	public static void main(String[] args) {
-		listAll(0, new File("D:\\storage"));
+//		listAll(0, new File("D:\\storage"));
+		sendEmail("947206869@qq.com", "111");
+		System.out.println("发送成功");
+	}
+
+
+	//	邮箱验证码
+	public static boolean sendEmail(String emailaddress, String code) {
+		try {
+			HtmlEmail email = new HtmlEmail();//不用更改
+			email.setHostName("smtp.126.com");//需要修改，126邮箱为smtp.126.com,163邮箱为163.smtp.com，QQ为smtp.qq.com
+			email.setCharset("UTF-8");
+			email.addTo(emailaddress);// 收件地址
+
+			email.setFrom("lyszhen3@126.com", "aa");//此处填邮箱地址和用户名,用户名可以任意填写
+             //此处填写邮箱地址和客户端授权码
+			email.setAuthentication("lyszhen3@126.com", "***");
+
+			email.setSubject("孙大大通讯");//此处填写邮件名，邮件名可任意填写
+			email.setMsg("尊敬的用户您好,您本次注册的验证码是:" + code);//此处填写邮件内容
+
+			email.send();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
