c1543ec5f48d84e6c6481a95e54b84f04654b323
lyszhen3 <lyszhen3@126.com>
2019-02-25 09:39:29 +0800

    maven plugin

--- hello-maven-plugin/src/main/java/org/lin/sample/plugin/GreetingMojo.java
+++ hello-maven-plugin/src/main/java/org/lin/sample/plugin/GreetingMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Created by lys on 2019/1/23.
@@ -15,8 +16,16 @@ import org.apache.maven.plugins.annotations.Mojo;
 
 @Mojo( name = "sayhi")
 public class GreetingMojo extends AbstractMojo {
+	/**
+	 * The greeting to display
+	 * 设置参数
+	 */
+	@Parameter(property = "sayhi.greeting", defaultValue = "Hello world")
+	private String greeting;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info( "Hello, world." );
+		getLog().info( "Hello, world." +greeting);
+		getLog().info("outPut greeting:"+greeting);
 	}
 }
