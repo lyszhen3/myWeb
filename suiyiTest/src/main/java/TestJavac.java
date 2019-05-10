d63389e0eec8512f2a255d75140cd68eb44c1d9f
lys <947206869@qq.com>
2019-05-10 18:01:46 +0800

    线上回滚

--- suiyiTest/src/main/java/TestJavac.java
+++ suiyiTest/src/main/java/TestJavac.java
@@ -1,6 +1,3 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * Created by lys on 2019/2/11.
@@ -12,13 +9,5 @@ import java.util.stream.IntStream;
 public class TestJavac {
     public static void main(String[] args) throws Exception {
 //		Main.main(new String[]{"D:\\mywork\\myWeb\\suiyiTest\\src\\main\\java\\ListToMap.java"});
-        List<Integer> collect = IntStream.range(0, 100).boxed().collect(Collectors.toList());
-        System.out.println(collect);
-        ;
-
-    }
-
-    public static void sayLoop() {
-        throw new RuntimeException("wo");
     }
 }
--- suiyiTest/src/main/java/git/TestHead.java
+++ suiyiTest/src/main/java/git/TestHead.java
@@ -1,6 +1,9 @@
 package git;
 
 /**
+ *
+ * git reset --hard <commit-id> 先把head回到某个commit
+ * git push -f  然后强制push。。 然后线上就回滚了
  * @author LinYuanSheng
  * @date 5/10/2019
  */
