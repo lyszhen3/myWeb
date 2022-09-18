872aa51babe58d63defd475e64e733c02d39d0b1
lys <947206869@qq.com>
2022-09-18 12:54:17 +0800

    红黑树,先写个概念吧

---
+++ suiyiTest/src/main/java/algorithm/BinaryTree/RedBlackTree.java
@@ -0,0 +1,17 @@
+package algorithm.BinaryTree;
+
+/**
+ * Created by lys on 2022/9/18.
+ *
+ * @author lys
+ * @version 3.0.0-SNAPSHOT
+ * @since 3.0.0-SNAPSHOT
+ * 红黑树
+ * 1.根节点和叶子节(包含null叶子节点.如果不包含null叶子节点.那么叶子节点可以为红色)点为黑色
+ * 2.不能有两个相连的红色节点
+ * 3.不同路径到父节点的黑色节点数量相同
+ *
+ * 2和3共同作用使得最长的路径只能是最短路径的两倍,从而达到一个相对平衡
+ */
+public class RedBlackTree {
+}
