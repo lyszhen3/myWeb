c89f7f609c59aad0fe022638dcdc3ed69a3c9928
wb-lys543834 <wb-lys543834@alibaba-inc.com>
2022-11-25 19:18:20 +0800

    z形排序

---
+++ suiyiTest/src/main/java/leetcode/algorithm/ZShapeSort.java
@@ -0,0 +1,33 @@
+package leetcode.algorithm;
+
+/**
+ * @author LinYuanSheng
+ * @date 2022/11/25
+ * 给定行数. z字排序后,从左往右输出
+ * e.g. 输入 "PAYPALISHIRING" 行数3
+ * P     A     H
+ * A  P  L  S  I
+ * Y     I     R     ....画不动了
+ *
+ * 输出 PAH......
+ */
+public class ZShapeSort {
+	public static void main(String[] args) {
+		String input = "PAYPALISHIRING";
+		int row = 3;
+
+		final String convert = convert(input, row);
+
+	}
+
+	/**
+	 * 通过二维数组实现?
+	 * @param input
+	 * @param row
+	 * @return
+	 */
+	private static String convert(String input, int row){
+
+		return null;
+	}
+}
