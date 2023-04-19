132af69b2967a17e9953365a3de56f1b5815cc73
lys <947206869@qq.com>
2023-04-19 22:27:16 +0800

    括号动态规划

--- suiyiTest/src/main/java/leetcode/algorithm/LongestPalindrome.java
+++ suiyiTest/src/main/java/leetcode/algorithm/LongestPalindrome.java
@@ -46,7 +46,7 @@ public class LongestPalindrome {
 	/**
 	 * 存在一个回文 aba 那么 左边-1=右边 +1 那么也是个回文,例如 cabac
 	 * 我们将a[l][r]=true表示回文保存起来,如此依赖a[l-1][r+1]也能轻松确定是否回文
-	 *
+	 * 动态规划还不如上面的来得快呢
 	 * @param s
 	 * @return
 	 */
@@ -60,44 +60,49 @@ public class LongestPalindrome {
 		}
 		boolean[][] pails = new boolean[s.length()][s.length()];
 
-		//先把i==j的存储起来和 i,i+1存起来
-		for (int i = 0; i < s.length(); i++) {
-			pails[i][i] = true;
-			if (i < s.length() -1) {
-				if (s.charAt(i) == s.charAt(i +1)) {
-					pails[i][i+1] = true;
-				}
-			}
-		}
+//		//先把i==j的存储起来和 i,i+1存起来
+//		for (int i = 0; i < s.length(); i++) {
+//			pails[i][i] = true;
+//		}
 		//假设从2开始
 		int maxL = 0;
 		//最终左边坐标
 		int fL = 0;
 		//最终右边坐标
 		int fR = 0;
-		for (int i = 0; i < s.length()-2; i++) {
 
-			for (int j = i + 2; j < s.length(); j++) {
-				if (s.charAt(i) == s.charAt(j) && pails[i + 1][j - 1]) {
-					pails[i][j] = true;
-					if (j-i > maxL) {
-						maxL = j - i;
-						fL = i;
-						fR = j;
-					}
-				}else {
-					pails[i][j] =false;
+		char[] charArray = s.toCharArray();
+
+		//遍历长度
+		for (int n = 2; n <=s.length(); n++) {
+
+			for (int l = 0; l < s.length(); l++) {
+				int r = l + n - 1;
+				if (r >= s.length()) {
+					break;
 				}
+				if (charArray[l] == charArray[r] && (n == 2 || l + 1 == r - 1 || pails[l + 1][r - 1])) {
+					pails[l][r] = true;
+					if (n > maxL) {
+						maxL = n;
+						fL = l;
+						fR = r;
+					}
+				} else {
+					pails[l][r] = false;
+				}
+
 			}
 		}
 
-		return s.substring(fL,fR +1);
+		return s.substring(fL, fR + 1);
 	}
 
 	public static void main(String[] args) {
 		LongestPalindrome longestPalindrome = new LongestPalindrome();
-		final String result = longestPalindrome.longestPalindrome("aaabcbadd");
-		final String result2 = longestPalindrome.longestPalindrome_2("aaabcbadd");
+		String s = "aaabcdddaddd";
+		final String result = longestPalindrome.longestPalindrome(s);
+		final String result2 = longestPalindrome.longestPalindrome_2(s);
 		System.out.println(result);
 		System.out.println(result2);
 	}
