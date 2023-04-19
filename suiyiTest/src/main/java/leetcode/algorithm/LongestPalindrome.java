efc92b9630bf4f1dbbefa7379f1791e85ced4c7c
wb-lys543834 <wb-lys543834@alibaba-inc.com>
2023-04-19 17:53:01 +0800

    想不明白啊

--- suiyiTest/src/main/java/leetcode/algorithm/LongestPalindrome.java
+++ suiyiTest/src/main/java/leetcode/algorithm/LongestPalindrome.java
@@ -6,6 +6,8 @@ package leetcode.algorithm;
  * @author lys
  * @version 3.0.0-SNAPSHOT
  * @since 3.0.0-SNAPSHOT
+ * 最长回文串
+ * aabacd  最长回文是 aba
  */
 public class LongestPalindrome {
 
@@ -41,9 +43,62 @@ public class LongestPalindrome {
 	}
 	//todo lys 如果用动态规划会怎么做呢?
 
+	/**
+	 * 存在一个回文 aba 那么 左边-1=右边 +1 那么也是个回文,例如 cabac
+	 * 我们将a[l][r]=true表示回文保存起来,如此依赖a[l-1][r+1]也能轻松确定是否回文
+	 *
+	 * @param s
+	 * @return
+	 */
+	public String longestPalindrome_2(String s) {
+
+		if (s == null) {
+			return null;
+		}
+		if (s.length() == 1) {
+			return s;
+		}
+		boolean[][] pails = new boolean[s.length()][s.length()];
+
+		//先把i==j的存储起来和 i,i+1存起来
+		for (int i = 0; i < s.length(); i++) {
+			pails[i][i] = true;
+			if (i < s.length() -1) {
+				if (s.charAt(i) == s.charAt(i +1)) {
+					pails[i][i+1] = true;
+				}
+			}
+		}
+		//假设从2开始
+		int maxL = 0;
+		//最终左边坐标
+		int fL = 0;
+		//最终右边坐标
+		int fR = 0;
+		for (int i = 0; i < s.length()-2; i++) {
+
+			for (int j = i + 2; j < s.length(); j++) {
+				if (s.charAt(i) == s.charAt(j) && pails[i + 1][j - 1]) {
+					pails[i][j] = true;
+					if (j-i > maxL) {
+						maxL = j - i;
+						fL = i;
+						fR = j;
+					}
+				}else {
+					pails[i][j] =false;
+				}
+			}
+		}
+
+		return s.substring(fL,fR +1);
+	}
+
 	public static void main(String[] args) {
 		LongestPalindrome longestPalindrome = new LongestPalindrome();
 		final String result = longestPalindrome.longestPalindrome("aaabcbadd");
+		final String result2 = longestPalindrome.longestPalindrome_2("aaabcbadd");
 		System.out.println(result);
+		System.out.println(result2);
 	}
 }
