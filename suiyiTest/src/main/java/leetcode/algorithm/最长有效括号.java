a726eda39a98ae8ba4ab1e9a41cd12c143373226
林元盛 <wb-lys543834@alibaba-inc.com>
2024-11-13 17:29:55 +0800

    提交

--- suiyiTest/src/main/java/leetcode/algorithm/最长有效括号.java
+++ suiyiTest/src/main/java/leetcode/algorithm/最长有效括号.java
@@ -13,7 +13,7 @@ public class 最长有效括号 {
 
 		最长有效括号 zui = new 最长有效括号();
 
-		String i ="()()()()";
+		String i = "())";
 		final int i1 = zui.longestValidParentheses(i);
 		System.out.println(i1);
 	}
@@ -21,10 +21,11 @@ public class 最长有效括号 {
 	/**
 	 * i 表示有效括号的下标
 	 * s[i] = ")" 时, 括号才有效
-	 *
+	 * <p>
 	 * s[i-1]="(" 时 dp[i] = dp[i-2] + 2;
+	 * <p>
+	 * s[i-1]=")" 时 如果 s[i - dp[i-1] -1] = "(" 时 dp[i] = dp[i-1] + dp[i - dp[i-1] -2] + 2
 	 *
-	 * s[i-1]=")" 时 如果 s[i - dp[i-2] -1] = "(" 时 dp[i] = dp[i-1] + dp[i - dp[i-1] -2] + 2
 	 * @param s
 	 * @return
 	 */
@@ -36,12 +37,42 @@ public class 最长有效括号 {
 
 		final int length = s.length();
 
+		int[] dp = new int[length];
 		int index = 0;
+		int max = 0;
 		while (index < length) {
+			final char ee = s.charAt(index);
+			if (')' == ee) {
+				if (index > 0) {
+					final char ss = s.charAt(index - 1);
+					if ('(' == ss) {
+						if (index > 1) {
+							dp[index] = dp[index - 2] + 2;
+						} else {
+							dp[index] = 2;
+						}
+					}
+					if (')' == ss) {
+						if (index > 1) {
+							if (index - dp[index - 1] > 0) {
 
+								final char pss = s.charAt(index - dp[index - 1] - 1);
+								if ('(' == pss) {
+									if (index - dp[index - 1] > 2) {
+										dp[index] = dp[index - 1] + dp[index - dp[index - 1] - 2] + 2;
+									} else {
+										dp[index] = dp[index - 1] + 2;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			max = Math.max(max, dp[index]);
 
-
+			index++;
 		}
-		return 0;
+		return max;
 	}
 }
