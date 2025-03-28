c6fb0ad6dc414ad4fb2189709cfbc6c2cb9e9740
林元盛 <wb-lys543834@taobao.com>
2025-03-28 18:07:33 +0800

    哎tmd还差一点

--- suiyiTest/src/main/java/leetcode/algorithm/通配符匹配.java
+++ suiyiTest/src/main/java/leetcode/algorithm/通配符匹配.java
@@ -14,31 +14,111 @@ public class 通配符匹配 {
 	public static void main(String[] args) {
 
 		通配符匹配 p = new 通配符匹配();
-		String s = "aa";
-		String ps = "?a";
+		String s = "ab";
+		String ps = "*?*?*";
 		final boolean match = p.isMatch(s, ps);
 		System.out.println(match);
 
 	}
 
+	/**
+	 * i=字符模式下标，j=字符串下标
+	 * {dp[i,j]= dp[i-1,j-1] & p[i]==s[j] | s[i] != ?,* }
+	 * {dp[i,j]= dp[i,j-1]||dp[i-1,j-1] & s[j] 存在 | p[i] == ?,* }， p[i]== *时 遍历所有s
+	 *
+	 * @param s
+	 * @param p
+	 * @return
+	 */
 	public boolean isMatch(String s, String p) {
 
+		if (s == null) {
+			return false;
+		}
+		if (p == null) {
+			return false;
+		}
 
-		if (s == null || s.isEmpty()) {
-			return false;
+		if (s.equals(p)) {
+			return true;
 		}
-		if (p == null || p.isEmpty()) {
-			return false;
-		}
+
 
 		final int pL = p.length();
+
+		if (pL == 0) {
+			return false;
+		}
+
 		final int sL = s.length();
 
-		int pI = 0;
-		for (int i = 0; i < sL; i++) {
-			final char c = p.charAt(i);
+		boolean hasXing = false;
+		//把s.length =0 加进来, 空串
+		boolean[][] dp = new boolean[pL][sL];
 
+		int si = 0;
+		boolean emptyMatch = true;
+		for (int i = 0; i < pL; i++) {
+
+			final char pc = p.charAt(i);
+
+			if (sL == 0) {
+				if (pc != '*') {
+					emptyMatch = false;
+				}
+				continue;
+			}
+			if (si == sL) {
+				if (pc == '*') {
+					dp[i][sL - 1] = dp[i - 1][sL - 1];
+				}
+				continue;
+			}
+			for (int j = si; j < sL; j++) {
+
+				if (pc == '*') {
+					hasXing = true;
+
+					if (i == 0) {
+						dp[i][j] = true;
+					} else {
+						dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i - 1][j - 1];
+					}
+				} else if (pc == '?') {
+					if (i == 0) {
+						dp[i][j] = true;
+					} else {
+						if (j == 0) {
+							dp[i][j] = dp[i - 1][j];
+						} else {
+							dp[i][j] = dp[i - 1][j - 1];
+						}
+					}
+				} else {
+
+					final char sc = s.charAt(j);
+					if (i == 0) {
+						dp[i][j] = (pc == sc);
+					} else {
+						if (j == 0) {
+							dp[i][j] = dp[i - 1][j] && (pc == sc);
+						} else {
+							dp[i][j] = dp[i - 1][j - 1] && (pc == sc);
+						}
+					}
+				}
+
+				if (!hasXing) {
+					break;
+				}
+			}
+			if (pc != '*') {
+				si++;
+			}
 		}
-		return false;
+		if (sL == 0) {
+			return emptyMatch;
+		}
+		return dp[pL - 1][sL - 1];
 	}
 }
