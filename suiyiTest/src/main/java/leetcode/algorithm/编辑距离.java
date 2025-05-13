930ec276726bf888f54dd82ea3e09903bcee08e2
lys <947206869@qq.com>
2025-05-13 20:37:26 +0800

    编辑距离提价

---
+++ suiyiTest/src/main/java/leetcode/algorithm/编辑距离.java
@@ -0,0 +1,21 @@
+package leetcode.algorithm;
+
+
+/**
+ * 以hourse(单词a) -> ros(单词b) 为例
+ * 1. 有新增,删除,替换三种操作
+ * 2. 两个单词操作就有6种
+ *  1. 单词a新增一个字符=单词b删除一个字符
+ *  2. 单词b新增一个字符=单词a删除一个字符
+ *  3. 单词a替换一个字符=单词b替换一个字符
+ * 
+ * 
+ */
+public class 编辑距离 {
+
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
