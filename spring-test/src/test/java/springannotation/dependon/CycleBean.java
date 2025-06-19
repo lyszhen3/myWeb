e712ddf9d7208d299453e1ce27b66e83aad0951c
林元盛 <wb-lys543834@taobao.com>
2025-06-19 10:06:55 +0800

    优化

--- spring-test/src/test/java/springannotation/dependon/CycleBean.java
+++ spring-test/src/test/java/springannotation/dependon/CycleBean.java
@@ -1,7 +1,6 @@
 package springannotation.dependon;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 /**
  * @author LinYuanSheng
